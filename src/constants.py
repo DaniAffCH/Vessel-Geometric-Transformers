fc5e584e027e5aca55f3ea1e2368f51c16b70ee4
DaniAffCH <danieleaffinita2000@gmail.com>
2024-05-12 14:20:00 +0200

    add dataset download and mypy for CI

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+DATASET_DRIVE_URL = (
+    "https://drive.google.com/drive/folders/18lNjZPYKLmd7w-UX7GwepHAy2R-3YP3W"
+)
+DATASET_NAME = "stead"
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,8 +1,20 @@
-# import os
-# import gdown
+import os
+
+import gdown
+
+from src.constants import DATASET_DRIVE_URL, DATASET_NAME
 
 
 def download_dataset(
-    installation_path: str = "", force_download: bool = False
-):
-    print("A")
+    download_path: str = "dataset", force_download: bool = False
+) -> None:
+
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
+
+    dataset_path = os.path.join(download_path, DATASET_NAME)
+
+    if not os.path.exists(dataset_path) or force_download:
+        gdown.download_folder(DATASET_DRIVE_URL, output=download_path)
+    else:
+        print(f"Dataset found at {dataset_path}")
