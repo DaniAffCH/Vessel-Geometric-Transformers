74c6eae399ad7d1b1ffc4f34b1db6e8a9ba8d8ab
DaniAffCH <danieleaffinita2000@gmail.com>
2024-07-27 15:46:32 +0200

    memory optimization and dataset split

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,5 +1,4 @@
 import os
-from copy import deepcopy
 from pathlib import Path
 from typing import Callable, Dict, List, Optional
 
@@ -7,6 +6,7 @@ import gdown
 import h5py
 import torch
 import tqdm
+from torch.utils.data import Subset
 from torch_geometric.data import Data, InMemoryDataset
 
 from config import DatasetConfig
@@ -25,10 +25,15 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
         super(VesselDataset, self).__init__(
             config.download_path, transform, pre_transform
         )
+        if os.path.exists(self.processed_paths[0]):
+            self.data, self.slices = torch.load(self.processed_paths[0])
+        else:
+            self.data = None
+            self.slices = None
 
     @property
     def raw_file_names(self) -> List[str]:
-        r"""
+        """
         The name of the files in the :obj:`self.raw_dir` folder that must be
         present in order to skip downloading. Acts as base property needed for
         `raw_paths`.
@@ -45,7 +50,7 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
 
     @property
     def processed_file_names(self) -> List[str]:
-        r"""The name of the files in the :obj:`self.processed_dir` folder that
+        """The name of the files in the :obj:`self.processed_dir` folder that
         must be present in order to skip processing. Acts as base property
         needed for `processed_paths`.
 
@@ -62,7 +67,7 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
             project_root, self.root, "processed", self.purpose
         )
         Path(data_root).mkdir(parents=True, exist_ok=True)
-        return [os.path.join(self.purpose, "data.pt")]
+        return [os.path.join(data_root, "data.pt")]
 
     @property
     def has_download(self) -> bool:
@@ -71,7 +76,6 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
     def download(self) -> None:
         if not os.path.exists(self.root):
             os.mkdir(self.root)
-
         gdown.download_folder(
             self.config.drive_url, output=self.config.download_path
         )
@@ -88,7 +92,7 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
         )
 
     def process_h5(self, h5_path: str) -> List[Data]:
-        data_list = list()
+        data_list = []
         with h5py.File(h5_path, "r") as f:
             for sample_name in tqdm.tqdm(f, desc=f"Loading {h5_path}"):
                 assert (
@@ -99,7 +103,7 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
         return data_list
 
     def process(self) -> None:
-        data_list = list()
+        data_list = []
 
         bifurcating_db_path = os.path.join(
             self.raw_paths[0], self.config.bifurcating_path
@@ -112,17 +116,10 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
         data_list.extend(self.process_h5(single_db_path))
 
         if self.pre_filter is not None:
-            # This is necessary to not allocate additional memory
-            i = 0
-            while i < len(data_list):
-                if not self.pre_filter(data_list[i]):
-                    del data_list[i]
-                else:
-                    i += 1
+            data_list = [data for data in data_list if self.pre_filter(data)]
 
         if self.pre_transform is not None:
-            for i in range(len(data_list)):
-                data_list[i] = self.pre_transform(data_list[i])
+            data_list = [self.pre_transform(data) for data in data_list]
 
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
@@ -130,23 +127,30 @@ class VesselDataset(InMemoryDataset):  # type: ignore[misc]
 
 def random_split(
     dataset: InMemoryDataset, ratios: List[float]
-) -> List[InMemoryDataset]:
-    # TODO: not complete yet!
+) -> List[Subset]:
     assert (
         sum(ratios) == 1.0
     ), "The dataset splits (train + val + test) must sum up to 1"
 
-    dataset = dataset.shuffle()
+    n_samples = len(dataset)
+    indices = list(range(n_samples))
 
+    split_indices = [int(ratio * n_samples) for ratio in ratios]
+    split_indices[-1] = n_samples - sum(split_indices[:-1])
+
+    start_idx = 0
     splitted_datasets = []
 
-    for i in range(len(ratios)):
-        splitted_datasets.append(deepcopy(dataset))
+    for size in split_indices:
+        end_idx = start_idx + size
+        subset_indices = indices[start_idx:end_idx]
+        splitted_datasets.append(Subset(dataset, subset_indices))
+        start_idx = end_idx
 
     return splitted_datasets
 
 
-def get_datasets(dataset_config: DatasetConfig) -> Dict[str, VesselDataset]:
+def get_datasets(dataset_config: DatasetConfig) -> Dict[str, Subset]:
     assert (
         dataset_config.train_size
         + dataset_config.val_size
@@ -154,7 +158,8 @@ def get_datasets(dataset_config: DatasetConfig) -> Dict[str, VesselDataset]:
         == 1.0
     ), "The dataset splits (train + val + test) must sum up to 1"
 
-    dataset: VesselDataset = VesselDataset(dataset_config, "complete")
+    dataset = VesselDataset(dataset_config, "complete")
+
     train_dataset, val_dataset, test_dataset = random_split(
         dataset,
         [
