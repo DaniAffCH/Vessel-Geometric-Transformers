add face feature, corrected masking, add features size limit

src/data/datamodule.py
@@ -13,6 +13,7 @@ from config.dataclasses import DatasetConfig
 from src.data.dataset import VesselDataset
 from src.data.vessel import Vessel
 from src.lib import (
+    PlaneGeometricAlgebra,
     PointGeometricAlgebra,
     ScalarGeometricAlgebra,
     TranslationGeometricAlgebra,
@@ -137,51 +138,78 @@ class VesselBatch:
     labels: Tensor = field(default_factory=Tensor)
 
 
-def collate_vessels(batch: List[Vessel]) -> VesselBatch:
+def collate_vessels(
+    batch: List[Vessel], pad_value: int = -9999999
+) -> VesselBatch:
     """
     Collates a batch of Vessel objects into padded tensors.
 
     Args:
         batch (List[Vessel]): A list of Vessel objects.
+        pad_value (int): The value used to indicate padding
 
     Returns:
-        Tuple[Tensor, Tensor]: A tuple containing the padded batch tensor
-        and the corresponding masks tensor.
+        VesselBatch: A VesselBatch object containing the padded batch tensor,
+        masks tensor, and labels tensor.
     """
 
     elem: Vessel = batch[0]
     assert isinstance(elem, (Vessel, Data)), "DataLoader found invalid type"
-    pad_value = -1
 
-    max_size = max(vessel.pos.shape[0] for vessel in batch)
+    SIZE_LIMIT = 100  # TODO: move to config
+
     padded_data: List[Tensor] = []
     masks: List[Tensor] = []
+    labels: List[int] = []
 
     for vessel in batch:
-        # normalizing to be within 0 and 1
+        # Normalizing the vessel data
         wss = normalize(vessel.wss)
         pos = normalize(vessel.pos)
         pressure = normalize(vessel.pressure)
+        face = normalize(vessel.face.T)
 
-        # extracing the geometric algebra elements
-        ga_wss = TranslationGeometricAlgebra.fromElement(wss)
-        ga_pos = PointGeometricAlgebra.fromElement(pos)
-        ga_pressure = ScalarGeometricAlgebra.fromElement(pressure)
-        tensor = torch.stack((ga_pos, ga_wss, ga_pressure))
+        # Extracting the geometric algebra elements
+        ga_elements = [
+            TranslationGeometricAlgebra.fromElement(wss),
+            PointGeometricAlgebra.fromElement(pos),
+            ScalarGeometricAlgebra.fromElement(pressure),
+            PlaneGeometricAlgebra.fromElement(face),
+        ]
 
-        # padding to the right with max_size-vessel.pos.shape[0] elements
-        padding = (0, 0, 0, max_size - vessel.pos.shape[0])
-        padded_tensor = F.pad(tensor, padding, "constant", pad_value)
-        padded_data.append(padded_tensor)
+        ga_elements = [
+            (
+                F.pad(
+                    tensor,
+                    (0, 0, 0, SIZE_LIMIT - tensor.size(0)),
+                    "constant",
+                    pad_value,
+                )
+                if tensor.size(0) < SIZE_LIMIT
+                else tensor[:SIZE_LIMIT]
+            )
+            for tensor in ga_elements
+        ]
 
-        # Mask: 1 for real data, 0 for padded data
-        mask = (padded_tensor != pad_value).float()
+        # Construct the tensor by stacking geometric algebra elements
+        tensor = torch.stack(ga_elements)
+
+        # Append the padded tensor to the padded_data list
+        padded_data.append(tensor)
+
+        # Create a mask tensor: 1 for real data, 0 for padded data
+        mask = (tensor != pad_value).float()
+        mask = mask[..., 0]
         masks.append(mask)
 
+        # Store the label of the vessel
+        labels.append(vessel.label.value)
+
+    # Stack the padded tensors, masks, and labels into batch tensors
     collated_batch = VesselBatch(
         data=torch.stack(padded_data),
         mask=torch.stack(masks),
-        labels=torch.tensor([vessel.label.value for vessel in batch]),
+        labels=torch.tensor(labels),
     )
 
     return collated_batch

src/models/baseline.py
@@ -23,7 +23,13 @@ class BaselineTransformer(L.LightningModule):  # type: ignore[misc]
             feedforward_dim=config.transformer_feedforward_dim,
             num_layers=config.transformer_num_layers,
         )
-        self.projection = nn.Linear(config.transformer_embedding_dim, 1)
+        SIZE_LIMIT = 100  # TODO: move to config
+
+        num_features = 4
+
+        self.projection = nn.Linear(
+            config.transformer_embedding_dim * SIZE_LIMIT * num_features, 1
+        )
         self.config = config
         self.loss_fn = nn.BCEWithLogitsLoss()
 
@@ -33,15 +39,24 @@ class BaselineTransformer(L.LightningModule):  # type: ignore[misc]
 
         Args:
             x (Tensor): Input tensor of shape
-                        (batch_size, seq_length, embed_dim).
+                        (batch_size, num_elements, seq_length, ga_size).
             mask (Tensor): Mask tensor of shape
-                           (batch_size, 1, seq_length, seq_length).
+                           (batch_size, num_elements, seq_length).
 
         Returns:
             Tensor: Output tensor with logits.
         """
+        # (batch_size, num_elements*seq_length, ga_size)
+        x = x.reshape(x.size(0), -1, x.size(-1))
+
+        # (batch_size, num_elements*seq_length)
+        mask = mask.reshape(x.size(0), -1)
+
         x = self.encoder(x, mask)
+        print(x.shape)
+        x = x.reshape(x.size(0), -1)
         x = self.projection(x)
+
         return x  # Logits are used directly for BCEWithLogitsLoss
 
     def training_step(self, batch: VesselBatch, batch_idx: int) -> Tensor:
@@ -75,6 +90,7 @@ class BaselineTransformer(L.LightningModule):  # type: ignore[misc]
             Tensor: Computed loss value.
         """
         logits = self(batch.data, batch.mask)
+        print(logits.squeeze().shape, batch.labels.shape)
         loss = self.loss_fn(logits.squeeze(), batch.labels)
         self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss

src/models/layers/default/transformer.py
@@ -68,7 +68,7 @@ class MultiHeadAttentionLayer(nn.Module):  # type: ignore[misc]
             torch.Tensor: The output tensor after applying
                           multi-head attention.
         """
-        batch_size = query.size(0)
+        batch_size, seq_length = query.size(0), query.size(1)
 
         query = self.q_proj(
             query
@@ -88,17 +88,27 @@ class MultiHeadAttentionLayer(nn.Module):  # type: ignore[misc]
         value = value.view(
             batch_size, -1, self.num_heads, self.head_dim
         ).transpose(1, 2)
-
         # Compute scaled dot-product attention
         scores = (
             torch.matmul(query, key.transpose(-2, -1)) / self.head_dim**0.5
         )  # Shape: (batch_size, num_heads, seq_length, seq_length)
-
         if mask is not None:
+
+            # Shape: (batch_size, seq_length)
+            # -> (batch_size, seq_length, seq_length)
+            mask = mask.unsqueeze(1) * mask.unsqueeze(2)
+
+            # Shape: (batch_size, seq_length, seq_length)
+            # -> (batch_size, num_heads, seq_length, seq_length)
+            mask = mask.unsqueeze(1).expand(
+                batch_size, self.num_heads, seq_length, seq_length
+            )
+
             # If a token is masked the model shouldn't pay attention to it
             scores = scores.masked_fill(mask == 0, float("-inf"))
 
         attn_weights = torch.nn.functional.softmax(scores, dim=-1)
+        print(attn_weights.shape, value.shape)
         attn_output = torch.matmul(
             attn_weights, value
         )  # Shape: (batch_size, num_heads, seq_length, head_dim)
@@ -109,7 +119,6 @@ class MultiHeadAttentionLayer(nn.Module):  # type: ignore[misc]
             .contiguous()
             .view(batch_size, -1, self.embed_dim)
         )
-
         output = self.out_proj(attn_output)
 
         return output
@@ -151,7 +160,7 @@ class TransformerEncoderLayer(nn.Module):  # type: ignore[misc]
             x (torch.Tensor): The input tensor of shape
                               (batch_size, seq_length, embed_dim).
             mask (torch.Tensor, optional): The attention mask tensor of shape
-                                    (batch_size, 1, seq_length, seq_length).
+                                    (batch_size, seq_length).
                                     Default is None.
 
         Returns: